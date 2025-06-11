3814ab4a829b2e099007d6c3e0145c2993cc173f
Tomas <tomasbeta@outlook.com>
2025-06-11 17:35:16 -0300

    Login activity implementado

---
+++ Android/BotonApplication/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.botonapplication"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.botonapplication"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.org.eclipse.paho.client.mqttv3)
+    implementation(libs.org.eclipse.paho.android.service)
+    implementation(libs.localbroadcastmanager)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ Android/BotonApplication/app/src/main/java/com/example/botonapplication/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.botonapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String USER_CORRECTO = "admin";
+    private static final String PASS_CORRECTO = "1234";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText inputUser = findViewById(R.id.inputUser);
+        EditText inputPass = findViewById(R.id.inputPass);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+            String user = inputUser.getText().toString().trim();
+            String pass = inputPass.getText().toString().trim();
+
+            if (user.equals(USER_CORRECTO) && pass.equals(PASS_CORRECTO)) {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Para que no se pueda volver con "Atrás"
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+

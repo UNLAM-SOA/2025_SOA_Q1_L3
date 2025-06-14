4a3f1f50bdcfa7ea30fce2f78d018e778cc6e1f0
Tomas <tomasbeta@outlook.com>
2025-06-14 15:23:13 -0300

    feat: Version MQTT basica funcionando

---
+++ Android/BotonApplication/app/src/main/java/com/example/botonapplication/mqtt/ConfigMQTT.java
@@ -0,0 +1,13 @@
+package com.example.botonapplication.mqtt;
+
+public class ConfigMQTT {
+
+    public static final String MQTT_SERVER_UBIDOTS = "tcp://industrial.api.ubidots.com:1883";
+    public static final String USER_NAME_UBIDOTS = "BBUS-GvjZYx9q0dvsWlYvjV2JNRBjHPWQzo";
+    public static final String USER_PASS_UBIDOTS = "BBUS-GvjZYx9q0dvsWlYvjV2JNRBjHPWQzo";
+
+    public static final String CLIENT_ID_UBIDOTS = "esp32_grupol3";
+    public static final String TOPIC_ALARMA_UBIDOTS = "/v1.6/devices/grupol3/alarma";
+    public static final String TOPIC_NIVEL_ALARMA_UBIDOTS = "/v1.6/devices/grupol3/nivel_alarma";
+
+}
